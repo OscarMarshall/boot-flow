fd3b5d643745898b731ba9d69735304c554e9c51
Oscar Marshall <oscar.lim.marshall@gmail.com>
2017-03-15 13:09:37 -0700

    Write finish with support for feature

--- src/penny_profit/boot_flow.clj
+++ src/penny_profit/boot_flow.clj
@@ -1,11 +1,17 @@
 (ns penny-profit.boot-flow
-  (:require [clj-jgit.porcelain :as git]
-            [clj-jgit.querying :as gitq]))
+  (:require [boot.core :as boot, :refer [deftask]]
+            [clj-jgit.internal :as giti]
+            [clj-jgit.porcelain :as git]
+            [clj-jgit.querying :as gitq]
+            [clojure.set :as set])
+  (:import (java.io FileNotFoundException)
+           (org.eclipse.jgit.api MergeCommand MergeCommand$FastForwardMode)
+           (org.eclipse.jgit.lib ObjectId Ref)))
 
 (set! *warn-on-reflection* true)
 
+(defn feature-finish [_] identity)
 (defn feature-start [_] identity)
-
 (defn feature-switch [_] identity)
 
 (defn clean? [repo]
@@ -56,4 +62,24 @@
               :else
               (do (git/git-checkout repo branch true false "develop")
                   (((feature-switch branch) handler) fileset))))
+          (throw (Exception. "Please commit or stash your changes")))))))
+
+(deftask finish []
+  (fn [handler]
+    (fn [fileset]
+      (let [repo (git/load-repo ".")]
+        (if (clean? repo)
+          (let [branch (git/git-branch-current repo)]
+            (case (nth (re-matches #"(feature)/.*" branch) 1)
+              "feature"
+              (do
+                (git/git-checkout repo "develop")
+                (.. repo
+                    merge
+                    (include ^ObjectId (giti/resolve-object branch repo))
+                    (setFastForward MergeCommand$FastForwardMode/NO_FF)
+                    (setMessage (str "Merge branch '" branch "' into develop"))
+                    call)
+                (git/git-branch-delete repo [branch])
+                (((feature-finish branch) handler) fileset))))
           (throw (Exception. "Please commit or stash your changes")))))))
