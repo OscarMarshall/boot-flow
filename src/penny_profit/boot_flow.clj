c778723c15722641be1676caba64be205d7d0a0f
Oscar Marshall <oscar.lim.marshall@gmail.com>
2017-03-15 11:34:52 -0700

    Write list-branches function

--- src/penny_profit/boot_flow.clj
+++ src/penny_profit/boot_flow.clj
@@ -1,1 +1,11 @@
-(ns penny-profit.boot-flow)
+(ns penny-profit.boot-flow
+  (:require [clj-jgit.porcelain :as git]))
+
+(defn list-branches [repo]
+  (into #{}
+        (comp (map (fn [^Ref ref]
+                     (nth (re-matches #"refs/heads/(.*)"
+                                      (.getName ref))
+                          1)))
+              (filter some?))
+        (git/git-branch-list repo)))
